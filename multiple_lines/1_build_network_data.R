5970db59fa503efce9289835987725c70672a56b
gguex <guillaume.guex@gmail.com>
2022-04-13 10:26:54 +0200

    tested build_network_structure

--- multiple_lines/1_build_network_data.R
+++ multiple_lines/1_build_network_data.R
@@ -19,12 +19,27 @@ source("local_functions.R")
 #--------------------------------
 
 # The file containing the dataset
-line_data_file = ""
+line_data_file = "multilines_data/bus_df.csv"
 # The file containing the pedestrian distance between stops
-d_file = ""
+d_file = "multilines_data/ped_time.csv"
+# Out folder
+out_folder = "network_data"
 
 
 #--------------------------------
 # Process
 #--------------------------------
+
+# Load the line file
+line_df = read.csv(line_data_file)
+# Load the distance file
+d_ped = as.matrix(read.csv(d_file, header=F))
+# Build the network structure 
+res_list = build_network_structure(interaction(line_df$line_nbr, 
+                                               line_df$direction), 
+                                   line_df$line_nbr,
+                                   d_ped,
+                                   180)
+
+
 
