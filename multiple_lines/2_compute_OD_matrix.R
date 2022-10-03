51833c0d2ecdd0e542449b1a579ec2a40dc08dfb
romainloup <romain.loup@unil.ch>
2022-10-03 13:30:26 +0200

    Data viz

--- multiple_lines/2_compute_OD_matrix.R
+++ multiple_lines/2_compute_OD_matrix.R
@@ -78,11 +78,26 @@ node_in_btw = colSums(x_res$x_btw)
 node_out_btw = rowSums(x_res$x_btw)
 
 # Print top transfers 
+agr_passengers = aggregate(line_df$passengers_in, list(line_df$line_nbr), FUN=sum)
+agr_passengers_stop = aggregate(line_df$passengers_in, list(line_df$line_nbr), FUN=sum) 
+
 top = sort(x_res$x_btw[x_res$x_btw > 0], decreasin=T)
 for(i in 1:10){
   ind_top = which(x_res$x_btw == top[i], arr.ind=T)
-  cat(line_df["stop_names"][ind_top[1],], "to", line_df["stop_names"][ind_top[2],], "with", top[i] ,"\n")
+  cat(line_df["stop_names"][ind_top[1],], "to", line_df["stop_names"][ind_top[2],], "with", top[i], "Tot stop:", line_df["passengers_out"][ind_top[1],],
+      "Prop.:", top[i]/line_df["passengers_out"][ind_top[1],]*100 ,"\n"
+    )
+
+  #   cat(line_df["stop_names"][ind_top[1],], "to", line_df["stop_names"][ind_top[2],], "with", top[i], "Tot stop:", line_df["passengers_out"][ind_top[1],],
+  #     "Prop.:", top[i]/sum(n_mat)*1000 ,"\n"
+  # )
 }
+
+line_df["passengers_in"][ind_top[1],]
+
+agr_passengers[which(agr_passengers == line_df["line_nbr"][ind_top[1],]),][2]
+line_df["line_nbr"][ind_top[1],]
+
 
 # Updated dataframe
 line_res = line_df
