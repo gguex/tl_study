9e1e06e022892a0a4795f4c0dddd4146ff52e4fc
gguex <guillaume.guex@gmail.com>
2023-01-19 16:23:50 +0100

    all lines multicore network building is ready

--- multiple_lines/1_build_network_data.R
+++ multiple_lines/1_build_network_data.R
@@ -23,9 +23,9 @@ source("local_functions.R")
 # # The file containing the dataset
 # line_data_file = "multilines_data/formatted_data/test_6789/bus_df.csv"
 # # The file containing the pedestrian distance between stops
-# d_file = "multilines_data/formatted_data/test_6789/ped_time.csv"
+# d_ped_file = "multilines_data/formatted_data/test_6789/ped_time.csv"
 # # Out folder
-# out_folder = "multilines_data/preprocessed_data/test_6789"
+# out_folder = "multilines_data/preprocessed_data/test_mc_6789"
 
 # --- ALL LINES
 
@@ -37,7 +37,7 @@ d_file = "multilines_data/formatted_data/all_lines/ped_time.csv"
 out_folder = "multilines_data/preprocessed_data/all_lines"
 
 # Pedestrian threshold for connecting stops
-d_ped_threshold = 120
+ped_t_thres = 120
 # The mean waiting time to enter a bus line
 waiting_time = 120
 
@@ -48,30 +48,45 @@ waiting_time = 120
 # Load the line file
 line_df = read.csv(line_data_file)
 # Load the distance file
-d_ped = as.matrix(read.csv(d_file, header=F))
+ped_t_mat = as.matrix(read.csv(d_file, header=F))
 # Save n
 n = dim(line_df)[1]
+# Line memberships 
+line_mbrshps = interaction(line_df$line_nbr, line_df$direction)
+# Tour memberships
+tour_mbrshps = line_df$line_nbr
 
 # Build the network structure 
-res_net_list = build_network_structure(interaction(line_df$line_nbr, 
-                                                   line_df$direction), 
-                                       line_df$line_nbr,
-                                       d_ped,
-                                       d_ped_threshold)
+res_net_list = build_network_structure(line_mbrshps, 
+                                       tour_mbrshps,
+                                       ped_t_mat,
+                                       ped_t_thres)
+
+# Travel time vec
+travel_t_vec = line_df$travel_time
+# Waiting time vec
+waiting_t_vec = rep(waiting_time, n)
+# Adjacency within
+adj_w = res_net_list$adj_w
+# Adjacency between
+adj_b = res_net_list$adj_b
+# Number of cores
+mc.cores = detectCores() - 2
+
+
 
 # Build the shortest path data
-res_sp_list = build_sp_data(interaction(line_df$line_nbr, 
-                                        line_df$direction), 
-                            line_df$line_nbr,
-                            line_df$travel_time,
-                            rep(waiting_time, n),
-                            d_ped, 
-                            res_net_list$adj_w,
-                            res_net_list$adj_b)
+res_sp_list = build_sp_data_mc(line_mbrshps, 
+                               tour_mbrshps,
+                               travel_t_vec,
+                               waiting_t_vec,
+                               ped_t_mat, 
+                               adj_w,
+                               adj_b, 
+                               mc.cores=mc.cores)
 
 # Build the corrected balance 
-res_inout_list = balance_flow_l_inout(interaction(line_df$line_nbr, 
-                                                  line_df$direction), 
+res_inout_list = balance_flow_l_inout(line_mbrshps,
                                       line_df$passengers_in, 
                                       line_df$passengers_out)
 
@@ -79,7 +94,14 @@ res_inout_list = balance_flow_l_inout(interaction(line_df$line_nbr,
 line_df["flow_l_in"] = res_inout_list$flow_l_in
 line_df["flow_l_out"] = res_inout_list$flow_l_out
 
-# Save data 
+#------------ Save data 
+
+# Create dir if it do not exist
+if(!dir.exists(out_folder)){
+  dir.create(out_folder)
+}
+
+# Write tables
 write.table(res_net_list$adj_w, paste0(out_folder, "/adj_w.csv"), sep=",",
             row.names=F, col.names=F)
 write.table(res_net_list$adj_b, paste0(out_folder, "/adj_b.csv"), sep=",",
