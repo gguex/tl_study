1749061f9b4c644bdb017280142a249bdc55a03c
gguex <guillaume.guex@gmail.com>
2023-02-06 17:56:59 +0100

    fix a bug in local_functions.R

--- local_functions.R
+++ local_functions.R
@@ -428,7 +428,7 @@ compute_origin_destination =
   function(flow_l_in, flow_l_out, edge_ref, sp_ref, sp_edge_link, 
            s_mat=NULL, min_p_ntwk=0.1, conv_thres_if=1e-5,
            conv_thres_algo=1e-5, epsilon=1e-40,
-           max_it=200, max_it_if=200, display_it=T){
+           max_it=200, max_it_if=400, display_it=T){
   
   # --- Get the network structure 
   
@@ -547,7 +547,7 @@ compute_origin_destination =
                              sp_ref[,1], sp_ref[,2])
     # Compute the updated g_vec
     reduced_flow = sp_flow_vec / path_max_ratio
-    g_ref_vec = reduced_flow / scaling_phi_psi
+    g_ref_vec = reduced_flow / (scaling_phi_psi + epsilon)
     g_ref_vec = g_ref_vec / sum(g_ref_vec)
     # Back to g_ref
     g_ref = as.matrix(sparseMatrix(i=sp_ref[, 1], j=sp_ref[, 2], 
