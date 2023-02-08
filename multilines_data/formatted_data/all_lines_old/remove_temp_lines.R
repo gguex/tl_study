4167b7ee1033b4182499400caf7a08bb0657a7e9
gguex <guillaume.guex@gmail.com>
2023-02-08 10:45:22 +0100

    remove very unbalanced lines

--- multilines_data/formatted_data/all_lines_old/remove_temp_lines.R
+++ multilines_data/formatted_data/all_lines_old/remove_temp_lines.R
@@ -1,16 +1,19 @@
-# Remove the temporary lines 81 and 82
+# Remove problematic lines
 
 # Set working directory path
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-# Indices to remove
-to_remove_ids = 1338:1360
 
 # Load data
 bus_df = read.csv("bus_df.csv")
 ped_time_mat = read.csv("ped_time.csv", header=F)
 
-# Remove idx
+# Line numbers to remove
+lines_to_remove = c(23, 29, 36, 41, 48, 54, 60, 81, 82)
+
+# Indices to remove
+to_remove_ids = which(bus_df$line_nbr %in% lines_to_remove)
+
+# Remove lines
 new_bus_df = bus_df[-to_remove_ids, ]
 new_ped_time_mat = ped_time_mat[-to_remove_ids, -to_remove_ids]
 
